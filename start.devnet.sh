e9d81c01a61de5f01cdb609eaccff692c1df12a5
wangyufsn <dev.wangyu@proton.me>
2023-11-24 14:46:57 +0800

    up

--- start.devnet.sh
+++ start.devnet.sh
@@ -5,7 +5,7 @@ docker run \
   -p 9782:80 \
   --name otterscan-devnet \
   -d \
-  --env ERIGON_URL="http://88.99.94.109:8745" \
-  --env BEACON_API_URL="http://88.99.94.109:4409" \
+  --env ERIGON_URL="http://88.99.94.109:10745" \
+  --env BEACON_API_URL="http://88.99.94.109:4400" \
   otterscan/otterscan:v2.2.0
 
